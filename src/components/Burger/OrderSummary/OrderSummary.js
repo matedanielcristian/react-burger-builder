a84ea1d54fe9e9dd12873d403c628419657d3623
Mate Daniel Cristian <matedanielcristian@gmail.com>
2021-02-10 17:36:59 +0200

    total price

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -24,6 +24,7 @@ const orderSumamry = (props) =>  {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total price: {props.price.toFixed(2)}</strong></p>
             <p>Continue the checkout</p>
             <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
             <Button  btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
